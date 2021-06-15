7352cbe6d7925d96a065763464809f95ab02504d
ZeusFtrOfc <77143853+ZeusFtrOfc@users.noreply.github.com>
2021-06-15 07:00:39 +0700

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -8,7 +8,7 @@ var zahirr = db.get("zahirr");
 	console.log('')  
 }
  
-var creator = "@ArifGans"
+var creator = "@ZeusXz"
 var secure = require('ssl-express-www');
 var cors = require('cors');
 var fetch = require('node-fetch');
